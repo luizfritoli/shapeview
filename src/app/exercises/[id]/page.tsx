2dc65fa3c71a8ddb069c4adae34bc99782973c0f
luizfritoli <luizgustavofritoli@gmail.com>
2025-08-06 12:40:59 -0300

    Colocada uma promise no dynamic route

--- src/app/exercises/[id]/page.tsx
+++ src/app/exercises/[id]/page.tsx
@@ -2,13 +2,11 @@ import ExerciseClient from "@/app/components/exerciseClient/ExerciseClient";
 import { exercises } from "@/lib/exercises";
 
 interface PageProps {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }
 
 export default async function ExercisePage({ params }: PageProps) {
-  const { id } = params;
+  const { id } = await params;
   const exercise = exercises.find((ex) => ex.id === id);
 
   if (!exercise) throw new Error("Depois eu resolvo!");
